b1057efe3640418f35f46057c06a052257308218
Amit Kumar <amit1711030007@gmail.com>
2024-01-04 12:54:26 +0530

    multer_fileupload_packageNodeJs

--- index.js
+++ index.js
@@ -2,25 +2,37 @@
 const express = require('express');
 require('./config');
 const Product = require('./product');
+const multer = require('multer');
 const app = express();
 
 app.use(express.json());
 
-app.get('/search/:key', async (request, response)=>{
-    const data = await Product.find({
-        "$or":[
-            {"name":{$regex:request.params.key}},
-            {"brand":{$regex:request.params.key}}
-        ]
-    });
-    console.log(request.params.key);
-    response.send(data);
-});
-// app.post('/create', async (request, response)=>{
-//     const data = new Product(request.body);
-//     const result = await data.save();
-//     response.send(result);
+const upload = multer({
+    storage: multer.diskStorage({
+        destination : function name(req, file, cb) {
+            cb(null, "fileUploads")
+        },
+        filename : function name(req, file, cb) {
+            cb(null, file.fieldname + "-"+Date.now()+ ".jpg")
+        }
+    })
+}).single("user_file");
+
+// app.get('/search/:key', async (request, response)=>{
+//     const data = await Product.find({
+//         "$or":[
+//             {"name":{$regex:request.params.key}},
+//             {"brand":{$regex:request.params.key}}
+//         ]
+//     });
+//     console.log(request.params.key);
+//     response.send(data);
 // });
+app.post('/upload', upload, async (request, response)=>{
+    // const data = new Product(request.body);
+    // const result = await data.save();
+    response.send("file Uploaded successfully");
+});
 // app.delete('/delete/:_id', async (request, response)=>{
 //     const data = new Product(request.params);
 //     const result= await data.deleteOne(request.params);
