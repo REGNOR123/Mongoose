8c56c20e4da20461622c0b2ed419ec21150bd47a
Amit Kumar <amit1711030007@gmail.com>
2024-01-04 12:32:43 +0530

    searchAPI

--- index.js
+++ index.js
@@ -6,24 +6,30 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', async (request, response)=>{
-    const data = await Product.find();
+app.get('/search/:key', async (request, response)=>{
+    const data = await Product.find({
+        "$or":[
+            {"name":{$regex:request.params.key}},
+            {"brand":{$regex:request.params.key}}
+        ]
+    });
+    console.log(request.params.key);
     response.send(data);
 });
-app.post('/create', async (request, response)=>{
-    const data = new Product(request.body);
-    const result = await data.save();
-    response.send(result);
-});
-app.delete('/delete/:_id', async (request, response)=>{
-    const data = new Product(request.params);
-    const result= await data.deleteOne(request.params);
-    response.send(result);
-});
-app.put('/update/:_id', async (request, response)=>{
-    const data = await Product.updateOne(
-        request.params,{$set:request.body}
-    );
-    response.send(data);
-});
+// app.post('/create', async (request, response)=>{
+//     const data = new Product(request.body);
+//     const result = await data.save();
+//     response.send(result);
+// });
+// app.delete('/delete/:_id', async (request, response)=>{
+//     const data = new Product(request.params);
+//     const result= await data.deleteOne(request.params);
+//     response.send(result);
+// });
+// app.put('/update/:_id', async (request, response)=>{
+//     const data = await Product.updateOne(
+//         request.params,{$set:request.body}
+//     );
+//     response.send(data);
+// });
 app.listen(5000);
